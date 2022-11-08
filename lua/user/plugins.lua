037ccbd5924132223170e7907c84371ee4568a32
Nuno Correia <nascorreia@gmail.com>
2022-11-08 18:04:52 +0000

    Added vim-kubernetes snippets

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,9 @@ return packer.startup(function(use)
 	-- Snippets
   use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
   use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+  use { "ycm-core/YouCompleteMe" }
+  use { "SirVer/ultisnips" }
+  use { "andrewstuart/vim-kubernetes" }
 
 	-- LSP
 	use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
